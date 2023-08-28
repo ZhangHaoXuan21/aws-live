d5f220d903b36392480e2f0473b41bf51f205394
ZhangHaoXuan21 <138643451+ZhangHaoXuan21@users.noreply.github.com>
2023-08-28 10:47:53 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cxcmimwuc6d8.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "jared-employee"
+customregion = "us-east-1"
+
